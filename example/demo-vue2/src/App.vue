a31176b0d0aa353d553072139cd042060bcf29e7
mao <maonianyou@foxmail.com>
2023-05-12 10:30:50 +0800

    doc: update demo-vue2

--- example/demo-vue2/src/App.vue
+++ example/demo-vue2/src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="app">
-    <grabbing-box>
+    <grabbing-box
+      :maxScale="300"
+      :minScale="10"
+      :scaleStep="10"
+      scaleButtonsPosition="bottom right"
+      :scrollSpeed="1"
+      :throttleSpan="20"
+    >
       <img alt="Vue logo" src="./assets/logo.png">
       <HelloWorld msg="Welcome to Your Vue.js App"/>
     </grabbing-box>
@@ -26,5 +33,7 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+
+  border: 1px solid gray;
 }
 </style>
