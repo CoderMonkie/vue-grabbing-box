b00b80a0238df31ce3a6d223a45951ce98c3f7ef
maonianyou <maonianyou@foxmail.com>
2023-05-15 22:20:54 +0800

    doc: example demo-vue2
    - improvement of demo (WYSIWYG)
    - auto deploy gh-pages on build demo-vue2

--- example/demo-vue2/vue.config.js
+++ example/demo-vue2/vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  outputDir: 'dist/demo-vue2',
+  publicPath: '/vue-grabbing-box/demo-vue2'
 })
