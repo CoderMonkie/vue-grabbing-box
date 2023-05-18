358f83e6a12c678d42e71af62fd3708c6d68d9c7
maonianyou <maonianyou@foxmail.com>
2023-05-19 00:43:41 +0800

    chore: prepare for gh-pages deploy
    make it posible to auto deploy for vue2 and vue3 demo

--- examples/demo-vue2/vue.config.js
+++ examples/demo-vue2/vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  outputDir: 'dist/demo-vue2',
+  outputDir: '../dist/demo-vue2',
   publicPath: '/vue-grabbing-box/demo-vue2'
 })
