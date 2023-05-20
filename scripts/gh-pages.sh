443bc2747f66d761ee1f68623116252d85d167be
maonianyou <maonianyou@foxmail.com>
2023-05-21 01:17:02 +0800

    chore: update action
    gh-pages

--- scripts/gh-pages.sh
+++ scripts/gh-pages.sh
@@ -1,3 +1,5 @@
+# 【用于本地 build:demo 时，通过 postbuild:demo 钩子，自动发布 gh-pages】
+# 【使用 action 自动发布时，作为 step：Push to repository】
 # 当前路径为执行脚本命令的路径，也就是 package.json 所在路径，而不是本文件的路径
 cd ./examples/dist
 git init
